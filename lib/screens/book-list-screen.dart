46d78fe054612952afb791bf4e4b7509757dd62e
sameldevt <samuelsouza.dev@outlook.com>
2024-12-28 15:07:40 -0300

    feat: dialog modificado

--- lib/screens/book-list-screen.dart
+++ lib/screens/book-list-screen.dart
@@ -26,157 +26,222 @@ class _BookListScreenState extends State<BookListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: null,
-        title: const Text(
-          'Livros lidos',
-          style: TextStyle(color: Colors.white),
-        ),
-        backgroundColor: Colors.deepPurple,
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showDialog(
-            context: context,
-            builder: (BuildContext context) {
-              return AlertDialog(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(6),
-                ),
-                title: const Text('Adicionar livro'),
-                content: SingleChildScrollView(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      SizedBox(
-                        width: double.maxFinite,
-                        child: Form(
-                          key: _formKey,
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              TextFormField(
-                                controller: _controller,
-                                decoration: InputDecoration(
-                                  hintText: "Digite o ISBN do livro",
-                                  labelText: 'ISBN',
-                                  border: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(6),
-                                  ),
-                                ),
-                                validator: (value) {
-                                  if (value == null || value.isEmpty) {
-                                    return 'O campo precisa ser preenchido';
-                                  }
-                                  return null;
-                                },
-                              ),
-                              const SizedBox(height: 6),
-                              InkWell(
-                                onTap: () async {
-                                  String isbn = _controller.text;
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(builder: (context) => _searchBook(isbn)),
-                                  );
-                                },
-                                child: Container(
-                                  decoration: BoxDecoration(
-                                    color: Colors.deepPurple,
-                                    borderRadius: BorderRadius.circular(6),
-                                  ),
-                                  width: double.maxFinite,
-                                  height: 46,
-                                  child: const Center(
-                                    child: Text(
-                                      'Buscar livro',
-                                      textAlign: TextAlign.center,
-                                      style: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 16,
-                                          fontWeight: FontWeight.bold),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              const SizedBox(height: 6),
-                              InkWell(
-                                onTap: () async {
-                                  final ImagePicker picker = ImagePicker();
-                                  final photo = await picker.pickImage(source: ImageSource.camera);
+      appBar: CustomAppBar(),
+      floatingActionButton: const AddBookFab(),
+      body: BookList(),
+    );
+  }
+}
 
-                                  if(photo != null){
-                                    var isbn = await _extractNumbers(File(photo.path));
-                                    _controller.text = isbn!;
-                                  }
-                                },
-                                child: Container(
-                                  decoration: BoxDecoration(
-                                    color: Colors.deepPurpleAccent,
-                                    borderRadius: BorderRadius.circular(6),
-                                  ),
-                                  width: double.maxFinite,
-                                  height: 46,
-                                  child: const Center(
-                                    child: Text(
-                                      'Tirar foto',
-                                      textAlign: TextAlign.center,
-                                      style: TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ],
+class CustomAppBar extends StatefulWidget implements PreferredSizeWidget{
+  const CustomAppBar({super.key});
+
+  @override
+  State<CustomAppBar> createState() => _CustomAppBarState();
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
+
+class _CustomAppBarState extends State<CustomAppBar> {
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      leading: null,
+      title: const Text(
+        'Livros lidos',
+        style: TextStyle(color: Colors.white),
+      ),
+      backgroundColor: Colors.deepPurple,
+    );
+  }
+}
+
+class AddBookFab extends StatefulWidget {
+  const AddBookFab({super.key});
+
+  @override
+  State<AddBookFab> createState() => _AddBookFabState();
+}
+
+class _AddBookFabState extends State<AddBookFab> {
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: () {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return const SearchBookDialog();
+          },
+        );
+      },
+      backgroundColor: Colors.deepPurple,
+      elevation: 10,
+      child: const Icon(
+        Icons.add,
+        color: Colors.white,
+      ),
+    );
+  }
+}
+
+class BookList extends StatefulWidget {
+  const BookList({super.key});
+
+  @override
+  State<BookList> createState() => _BookListState();
+}
+
+class _BookListState extends State<BookList> {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<Book>>(
+      future: BookService().loadBooks(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        if (snapshot.hasError) {
+          print(snapshot.stackTrace);
+          return Center(
+              child: Text('Error: ${snapshot.error}'));
+        }
+
+        if (!snapshot.hasData || snapshot.data == null) {
+          return const Center(
+              child: Text('No data available'));
+        }
+
+        final items = snapshot.data;
+
+        return ListView.builder(
+          itemCount: items!.length,
+          itemBuilder: (context, index) {
+            return BookCard(book: items[index]);
+          },
+        );
+      },
+    );
+  }
+}
+
+
+class SearchBookDialog extends StatefulWidget {
+  const SearchBookDialog({super.key});
+
+  @override
+  State<SearchBookDialog> createState() => _SearchBookDialogState();
+}
+
+class _SearchBookDialogState extends State<SearchBookDialog> {
+  final TextEditingController _controller = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  Book? book;
+  File? selectedMedia;
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(6),
+      ),
+      title: const Text('Adicionar livro'),
+      content: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            SizedBox(
+              width: double.maxFinite,
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    TextFormField(
+                      controller: _controller,
+                      decoration: InputDecoration(
+                        hintText: "Digite o ISBN do livro",
+                        labelText: 'ISBN',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(6),
+                        ),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'O campo precisa ser preenchido';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 6),
+                    InkWell(
+                      onTap: () async {
+                        String isbn = _controller.text;
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => _searchBook(isbn)),
+                        );
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.deepPurple,
+                          borderRadius: BorderRadius.circular(6),
+                        ),
+                        width: double.maxFinite,
+                        height: 46,
+                        child: const Center(
+                          child: Text(
+                            'Buscar livro',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold),
                           ),
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                    const SizedBox(height: 6),
+                    InkWell(
+                      onTap: () async {
+                        final ImagePicker picker = ImagePicker();
+                        final photo = await picker.pickImage(source: ImageSource.camera);
+
+                        if(photo != null){
+                          var isbn = await _extractNumbers(File(photo.path));
+                          _controller.text = isbn!;
+                        }
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.deepPurpleAccent,
+                          borderRadius: BorderRadius.circular(6),
+                        ),
+                        width: double.maxFinite,
+                        height: 46,
+                        child: const Center(
+                          child: Text(
+                            'Tirar foto',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              );
-            },
-          );
-        },
-        elevation: 10,
-        backgroundColor: Colors.deepPurple,
-        child: const Icon(
-          Icons.add,
-          color: Colors.white,
+              ),
+            ),
+          ],
         ),
-      ),
-      body: FutureBuilder<List<Book>>(
-        future: BookService().loadBooks(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-
-          if (snapshot.hasError) {
-            print(snapshot.stackTrace);
-            return Center(
-                child: Text('Error: ${snapshot.error}'));
-          }
-
-          if (!snapshot.hasData || snapshot.data == null) {
-            return const Center(
-                child: Text('No data available'));
-          }
-
-          final items = snapshot.data;
-
-          return ListView.builder(
-            itemCount: items!.length,
-            itemBuilder: (context, index) {
-              return BookCard(book: items[index]);
-            },
-          );
-        },
       ),
     );
   }
